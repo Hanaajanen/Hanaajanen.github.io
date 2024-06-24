51c150d55af7cde96609db6c938258c68f83a90c
花菱友絵 <108884112+Hanaajanen@users.noreply.github.com>
2024-06-24 12:53:45 +0800

    Update scripts.js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,13 +4,13 @@
 
         // 根据月份设置背景图片
         if (month >= 2 && month <= 4) { // 春季: 3月, 4月, 5月
-            backgroundImage = './src/bg/spring.jpg';
+            backgroundImage = '../bg/spring.jpg';
         } else if (month >= 5 && month <= 7) { // 夏季: 6月, 7月, 8月
-            backgroundImage = './src/bg/summer.jpg';
+            backgroundImage = '../bg/summer.jpg';
         } else if (month >= 8 && month <= 10) { // 秋季: 9月, 10月, 11月
-            backgroundImage = './src/bg/autumn.jpg';
+            backgroundImage = '../bg/autumn.jpg';
         } else { // 冬季: 12月, 1月, 2月
-            backgroundImage = './src/bg/winter.jpg';
+            backgroundImage = '../bg/winter.jpg';
         }
 
         // 设置背景图片
